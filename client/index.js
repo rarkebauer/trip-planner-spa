fc947723f443af095ec7671a8defb6d5903db5fd
Rachel Arkebauer <rarkebauer@gmail.com>
2017-11-22 12:23:37 -0500

    looping through options

--- client/index.js
+++ client/index.js
@@ -17,18 +17,19 @@ const map = new mapboxgl.Map({
 const marker = buildMarker("activities", fullstackCoords);
 marker.addTo(map);
 
+let hotels;
+let restaurants;
+let activities;
+hotels = document.getElementById('hotels-choices');
+restaurants = document.getElementById('restaurants-choices');
+activities = document.getElementById('activities-choices');
+let latlong = [0,0];
 fetch('/api')
   .then(result => {
     //console.log(result)
     return result.json()
   }).then(data => {
     //console.log(data)
-    let hotels;
-    let restaurants;
-    let activities;
-    hotels = document.getElementById('hotels-choices');
-    restaurants = document.getElementById('restaurants-choices');
-    activities = document.getElementById('activities-choices');
 
     data.hotels.forEach(hotel => {
       let op = document.createElement('option');
@@ -45,36 +46,84 @@ fetch('/api')
       op.innerHTML = `${activity.name}`
       activities.append(op);
     })
-  })
+    return data
+  }).then(data=> {
+     var adds = document.getElementsByClassName('options-btn');
+     for(var i=0; i<adds.length; i++){
+      console.log(adds[i]);
+       adds[i].addEventListener('click', ()=>{
+        let value = (adds[i].value)
+        const li = document.createElement('li');
+        li.innerHTML = value;
+        //call some function to handle individual lists
+        //myHotel.append(li);
+       })
+       }
+     })
   .catch(console.error)
 
-const selectHotel = document.getElementById(`hotels-choices`);
-const myHotel = document.getElementById('hotels-list')
-const addHotel = document.getElementById('hotels-add')
-  .addEventListener('click', () => {
-    let value = (selectHotel.value)
-    const li = document.createElement('li');
-    li.innerHTML = value;
-    myHotel.append(li);
-    const hotelMarker = buildMarker()
-  })
+// const selectHotel = document.getElementById(`hotels-choices`);
+// const myHotel = document.getElementById('hotels-list')
+// const addHotel = document.getElementById('hotels-add')
+//   .addEventListener('click', () => {
+//     let value = (selectHotel.value)
+//     const li = document.createElement('li');
+//     li.innerHTML = value;
+//     myHotel.append(li);
+//   })
+// console.log(data)
+// console.log(data.activities[0].place.location)
+// //selectRestaurant.selectedIndex
+// hotels, res
+// const selected = document.getElementById(`hotels-choices`);
+// console.log(selected)
+// const myHotel = document.getElementById('hotels-list')
+// const addHotel = document.getElementById('hotels-add')
+//   .addEventListener('click', () => {
+//     console.log(c)
+//     let value = (selected.value)
+//     const li = document.createElement('li');
+//     li.innerHTML = value;
+//     myHotel.append(li);
+//   })
+// //const markerH = selected.selectedIndex
+// buildMarker("hotels", data.hotels[selected.selectedIndex].place.location).addTo(map)
 
-const selectRestaurant = document.getElementById(`restaurants-choices`);
-const myRestaurant = document.getElementById('restaurants-list')
-const addRest = document.getElementById('restaurants-add')
-  .addEventListener('click', () => {
-    let value = (selectRestaurant.value)
-    const li = document.createElement('li');
-    li.innerHTML = value;
-    myRestaurant.append(li);
-  })
-
-const selectActivities = document.getElementById(`activities-choices`);
-const myActivities = document.getElementById('activities-list')
-const addAct = document.getElementById('activities-add')
-  .addEventListener('click', () => {
-    let value = (selectActivities.value)
-    const li = document.createElement('li');
-    li.innerHTML = value;
-    myActivities.append(li);
-  })
+// .then(data=> {
+//   console.log(data)
+//   console.log(data.activities[0].place.location)
+//   //selectRestaurant.selectedIndex
+//   const selectHotel = document.getElementById(`hotels-choices`);
+//   const myHotel = document.getElementById('hotels-list')
+//   const addHotel = document.getElementById('hotels-add')
+//     .addEventListener('click', () => {
+//       let value = (selectHotel.value)
+//       const li = document.createElement('li');
+//       li.innerHTML = value;
+//       myHotel.append(li);
+//     })
+//   //const markerH = selectHotel.selectedIndex
+//   buildMarker("hotels", data.hotels[selectHotel.selectedIndex].place.location).addTo(map)
+// }).then(data => {
+//   const selectRestaurant = document.getElementById(`restaurants-choices`);
+//   const myRestaurant = document.getElementById('restaurants-list')
+//   const addRest = document.getElementById('restaurants-add')
+//     .addEventListener('click', () => {
+//       let value = (selectRestaurant.value)
+//       const li = document.createElement('li');
+//       li.innerHTML = value;
+//       myRestaurant.append(li);
+//     })
+//     buildMarker("restaurants", data.restaurants[selectRestaurant.selectedIndex].place.location).addTo(map)
+// }).then(data => {
+//   const selectActivities = document.getElementById(`activities-choices`);
+//   const myActivities = document.getElementById('activities-list')
+//   const addAct = document.getElementById('activities-add')
+//     .addEventListener('click', () => {
+//       let value = (selectActivities.value)
+//       const li = document.createElement('li');
+//       li.innerHTML = value;
+//       myActivities.append(li);
+//     })
+//     buildMarker("activities", data.activities[selectActivities.selectedIndex].place.location).addTo(map)
+// })
